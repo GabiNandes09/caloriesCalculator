0edf5e18bf44833ce4f09b6e1bf51137b8e8de99
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-03-13 10:11:35 -0300

    Criação do DefaultTextValues

---
+++ app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/defaultComponents/DefaultTextValues.kt
@@ -0,0 +1,43 @@
+package com.gabrielfernandes.caloriescalculator.ui.defaultComponents
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DefaultTextValues(
+    modifier: Modifier = Modifier,
+    title: String,
+    value: String
+) {
+    Row(
+        modifier = modifier
+    ) {
+        Text(
+            text = "${title.uppercase()}: ",
+            color = Color.Black,
+            modifier = Modifier.padding(end = 10.dp)
+        )
+        Text(
+            text = value,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    DefaultTextValues(
+        title = "Título",
+        value = "123456"
+    )
+}
