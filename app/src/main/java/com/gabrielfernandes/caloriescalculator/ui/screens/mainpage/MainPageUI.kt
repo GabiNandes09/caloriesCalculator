13c4be1d2044523f750e52393d13cc38bceb8a54
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-03-13 10:40:15 -0300

    Organizando MainPageUI

--- app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/screens/mainpage/MainPageUI.kt
+++ app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/screens/mainpage/MainPageUI.kt
@@ -6,12 +6,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gabrielfernandes.caloriescalculator.ui.defaultComponents.DefaultComboBox
+import com.gabrielfernandes.caloriescalculator.ui.defaultComponents.DefaultTextField
 import com.gabrielfernandes.caloriescalculator.ui.defaultComponents.DefaultTextValues
 
 @Composable
@@ -19,6 +24,8 @@ fun MainPageUI() {
     Scaffold(
         containerColor = Color.Black
     ) { paddingValues ->
+        var requiredValue by remember { mutableStateOf("") }
+
         Column(
             modifier = Modifier
                 .padding(paddingValues)
@@ -26,15 +33,66 @@ fun MainPageUI() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            DefaultComboBox(
+            FirstItem(
                 itens = emptyList(),
-                modifier = Modifier.padding(horizontal = 25.dp, vertical = 10.dp)
-            ) {
-                
-            }
-            DefaultTextValues(title = "Calorias", value = "259 kcal")
-            DefaultTextValues(title = "Preço", value = "R$ 5,99")
+                onItemClick = {},
+                valueLabel = requiredValue,
+                onValueChange = { newValue ->
+                    requiredValue = newValue
+                },
+                itemSelected = null
+            )
+            SecondItem(
+                itens = emptyList(),
+                onItemClick = {},
+                itemSelected = null
+            )
         }
+    }
+}
+
+@Composable
+private fun FirstItem(
+    itens: List<Any>,
+    onItemClick: (Any) -> Unit,
+    valueLabel: String,
+    onValueChange: (String) -> Unit,
+    itemSelected: Any?
+) {
+    DefaultComboBox(
+        itens = itens,
+        label = "Selecione um item",
+        modifier = Modifier.padding(horizontal = 25.dp, vertical = 10.dp)
+    ) { item ->
+        onItemClick(item)
+    }
+    DefaultTextField(
+        modifier = Modifier.padding(horizontal = 25.dp, vertical = 10.dp),
+        label = valueLabel
+    ) { newValue ->
+        onValueChange(newValue)
+    }
+    if (itemSelected != null) {
+        DefaultTextValues(title = "Calorias", value = "259 kcal")
+        DefaultTextValues(title = "Preço", value = "R$ 5,99")
+    }
+}
+@Composable
+private fun SecondItem(
+    itens: List<Any>,
+    onItemClick: (Any) -> Unit,
+    itemSelected: Any?
+) {
+    DefaultComboBox(
+        itens = itens,
+        label = "Selecione um item",
+        modifier = Modifier.padding(horizontal = 25.dp, vertical = 10.dp)
+    ) { item ->
+        onItemClick(item)
+    }
+    if (itemSelected != null) {
+        DefaultTextValues(title = "Calorias", value = "259 kcal")
+        DefaultTextValues(title = "Preço", value = "R$ 5,99")
     }
 }
 
