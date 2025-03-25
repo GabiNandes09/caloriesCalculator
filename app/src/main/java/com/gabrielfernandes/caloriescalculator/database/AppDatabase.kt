b52b143eb996941955c2aa2fb6af3d159f7cca09
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-03-25 16:53:22 -0300

    Preparação do Room

---
+++ app/src/main/java/com/gabrielfernandes/caloriescalculator/database/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.gabrielfernandes.caloriescalculator.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+}
