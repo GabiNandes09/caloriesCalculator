6cfca3ec292de0240fa766a5ecbfb4c0b9b3aadd
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-04-02 10:54:17 -0300

    Correções

--- app/src/main/java/com/gabrielfernandes/caloriescalculator/MainActivity.kt
+++ app/src/main/java/com/gabrielfernandes/caloriescalculator/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.gabrielfernandes.caloriescalculator.ui.screens.mainpage.MainPageUI
 import com.gabrielfernandes.caloriescalculator.ui.theme.CaloriesCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CaloriesCalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainPageUI()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CaloriesCalculatorTheme {
-        Greeting("Android")
     }
 }
