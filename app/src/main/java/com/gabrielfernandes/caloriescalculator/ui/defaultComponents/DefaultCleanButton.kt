f3e6896489bb30333adad90ec85d67958cc0510e
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-04-03 16:44:46 -0300

    Adicionado possiveis cores Green e Red

--- app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/defaultComponents/DefaultCleanButton.kt
+++ app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/defaultComponents/DefaultCleanButton.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.gabrielfernandes.caloriescalculator.R
 
 @Composable
 fun DefaultCleanButton(
@@ -22,7 +24,7 @@ fun DefaultCleanButton(
     Column(modifier = modifier) {
         Button(
             onClick = { onCleanClick() },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+            colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.red))
         ) {
             Text(text = "LIMPAR", fontSize = 20.sp, modifier = Modifier.padding(10.dp))
         }
