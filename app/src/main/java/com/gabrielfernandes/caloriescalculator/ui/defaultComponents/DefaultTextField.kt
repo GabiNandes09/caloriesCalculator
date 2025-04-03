06d61d60603e1e6856201259ce7a67b7adcb1587
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-04-03 17:09:38 -0300

    Melhoria de layout DefaultTextField

--- app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/defaultComponents/DefaultTextField.kt
+++ app/src/main/java/com/gabrielfernandes/caloriescalculator/ui/defaultComponents/DefaultTextField.kt
@@ -1,5 +1,6 @@
 package com.gabrielfernandes.caloriescalculator.ui.defaultComponents
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DefaultTextField(
@@ -32,13 +34,13 @@ fun DefaultTextField(
                 value = newValue
                 onValueChange(newValue)
             },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().border(2.dp, Color.Black),
             label = {
                 Text(text = label)
             },
             colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.Gray,
-                unfocusedContainerColor = Color.Gray,
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
                 unfocusedTextColor = Color.Black,
                 focusedTextColor = Color.Black,
                 unfocusedLabelColor = Color.Black,
